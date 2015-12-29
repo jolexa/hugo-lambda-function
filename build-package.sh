30365c0c214a3783d0f2902dcecec42c25520832
Jeremy Olexa <jolexa@jolexa.net>
2015-12-29 03:12:51 +0000

    recursively build zip

--- build-package.sh
+++ build-package.sh
@@ -10,8 +10,9 @@ tar -xf hugo_0.15_linux_amd64.tar.gz
 rm -f hugo_0.15_linux_amd64.tar.gz
 mv hugo_0.15_linux_amd64/hugo_0.15_linux_amd64 hugo_0.15_linux_amd64.go
 rm -rf hugo_0.15_linux_amd64
+find . -name "*.pyc" -delete
 # wget from repo here TODO
-zip -9 /tmp/bundle.zip *
+zip -r9 /tmp/bundle.zip *
 cd ..
-zip -r9 /tmp/bundle.zip main.py
+zip -9 /tmp/bundle.zip main.py
 rm -rf $DIR
