a449cfc9e5565305fa50ff415250d2235e3bed02
Jeremy Olexa <jolexa@jolexa.net>
2016-07-23 16:10:20 -0500

    Almost working inline version of the script
    Still need to sort of the content-type as everything is uploaded at
    binary

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from __future__ import print_function
 
 import json
 import logging
+import boto3
 
 import urllib
 import zipfile
+import tarfile
 import subprocess
+import os
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -17,15 +20,43 @@ def lambda_handler(event, context):
 
     repourl = message['repository']['url']
     reponame = message['repository']['name']
-
     logger.info("This is the URL: " + str(repourl))
 
-    urllib.urlretrieve (repourl + "/archive/master.zip", "/tmp/master.zip")
+    # Unzip the github repo
+    logger.info("Fetching Repo: " + repourl + "/archive/master.zip" )
+    urllib.urlretrieve(repourl + "/archive/master.zip", "/tmp/master.zip")
     zfile = zipfile.ZipFile('/tmp/master.zip')
     zfile.extractall("/tmp/unzipped")
     builddir = "/tmp/unzipped/" + reponame + "-master/"
 
-    subprocess.call("/var/task/hugo_0.15_linux_amd64.go" , shell=True, cwd=builddir)
+    # Fetch hugo and call it
+    logger.info("Fetching hugo")
+    urllib.urlretrieve("https://github.com/spf13/hugo/releases/download/v0.15/hugo_0.15_linux_amd64.tar.gz", "/tmp/hugo.tar.gz")
+    tfile = tarfile.open('/tmp/hugo.tar.gz')
+    tfile.extractall("/tmp/hugo")
+    logger.info("Running hugo")
+    subprocess.call("/tmp/hugo/hugo_0.15_linux_amd64/hugo_0.15_linux_amd64" , shell=True, cwd=builddir)
+
+    # push to s3
+    logger.info("Pushing to s3")
     pushdir = builddir + "public/"
     bucketuri = "s3://" + reponame + "/"
-    subprocess.call("python /var/task/awscli.py s3 sync --size-only --delete --sse AES256 " + pushdir + " " + bucketuri, shell=True)
+    # Grr, I cannot believe there is no "sync" in boto3...https://github.com/boto/boto3/issues/358
+    client = boto3.client('s3')
+    os.chdir(pushdir)
+    for root, dirs, files in os.walk('.'):
+        for file in files:
+            f=os.path.join(root,file)
+            logger.info(f)
+            logger.info("Pushing file: " + f + " to: " + bucketuri)
+            body = open(f).read()
+
+
+            reponse = client.put_object(
+                Bucket=reponame,
+                Body=body,
+                Key=f.strip("./"),
+                StorageClass='REDUCED_REDUNDANCY',
+                ServerSideEncryption='AES256',
+                #ContentEncoding=
+                )
