a4099a1610b04864ce733c5235029d27b04c72e6
Abdelrahman Ahmed <abdelm.is@gmail.com>
2016-11-26 05:24:01 +1100

    Updated hugo version, added global var for version

--- build-package.sh
+++ build-package.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
 DIR=$(mktemp -d)
+HUGO_VERSION=0.17
+
 cd $DIR
 
 # Install awscli python lib + deps in buildroot
 pip install awscli -t .
+
 # Fetch aws cli wrapper from github
-wget https://raw.githubusercontent.com/aws/aws-cli/1.9.15/bin/aws \
+wget https://raw.githubusercontent.com/aws/aws-cli/develop/bin/aws \
 	-O awscli.py
+
 # Fetch hugo release (statically compile go binary)
-wget https://github.com/spf13/hugo/releases/download/v0.15/hugo_0.15_linux_amd64.tar.gz
-tar -xf hugo_0.15_linux_amd64.tar.gz
-rm -f hugo_0.15_linux_amd64.tar.gz
-mv hugo_0.15_linux_amd64/hugo_0.15_linux_amd64 hugo_0.15_linux_amd64.go
-rm -rf hugo_0.15_linux_amd64
+wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
+tar -xf hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
+rm -f hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
+mv hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 hugo.go
+rm -rf hugo_${HUGO_VERSION}_linux_amd64
+
 # cleanup
 find . -name "*.pyc" -delete
+
 # Fetch the main script from this repo
-wget https://raw.githubusercontent.com/jolexa/hugo-lambda-function/master/main.py
+wget https://raw.githubusercontent.com/abdel/hugo-lambda-function/master/main.py
+
 # create zip
 zip -r9 /tmp/hugo-lambda-function.zip *
 cd ..
+
 # cleanup
 rm -rf $DIR
